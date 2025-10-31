aa0c04c9b8f60a6691b9b62c6c069226afff00e1
Dylan-Ay <dylan.ayache.68@gmail.com>
2025-10-31 16:32:42 +0100

    Modifications du render

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,10 +1,48 @@
 import './components/ForecastSummary.js';
+import './components/SearchResult.js';
 
 const cityInput = document.getElementById('city-input');
 const meteoContainer = document.getElementById('meteo-container');
 
 let typingTimer;
 const debounceDelay = 500;
+
+// cityInput.addEventListener("input", (event) => {
+//   const city = cityInput.value.trim();
+//   if (city) {
+//     if (meteoContainer.hasChildNodes()) {
+//       meteoContainer.removeChild(meteoContainer.firstChild);
+//     }
+//     clearTimeout(typingTimer);
+//     typingTimer = setTimeout(() => {
+//       window.weatherAPI.getCurrentWeatherByCity(city)
+//       .then(data => {
+//         const forecastSummary = document.createElement('forecast-summary');
+
+//         forecastSummary.data = {
+//           icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+//           currentTemp: data.main.temp,
+//           feelsLike: data.main.feels_like,
+//           cityName: data.name,
+//           weather: data.weather,
+//           windSpeed: data.wind.speed,
+//           humidity: data.main.humidity,
+//           sunrise: data.sys.sunrise,
+//           sunset: data.sys.sunset,
+//           pressure: data.main.pressure,
+//           country: data.sys.country
+//         };
+
+//         meteoContainer.appendChild(forecastSummary);
+//         console.log(data)
+//       })
+//       .catch(err => {
+//         console.error('Erreur:', err);
+//       });
+//     }, debounceDelay);
+//   }
+// });
+
 
 cityInput.addEventListener("input", (event) => {
   const city = cityInput.value.trim();
@@ -14,25 +52,15 @@ cityInput.addEventListener("input", (event) => {
     }
     clearTimeout(typingTimer);
     typingTimer = setTimeout(() => {
-      window.weatherAPI.getCurrentWeatherByCity(city)
+      window.weatherAPI.getCities(city)
       .then(data => {
-        const forecastSummary = document.createElement('forecast-summary');
+        let newList = document.createElement('ul');
+      
+        const searchResult = document.createElement('search-result');
+        searchResult.data = data.features;
 
-        forecastSummary.data = {
-          icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-          currentTemp: data.main.temp,
-          feelsLike: data.main.feels_like,
-          cityName: data.name,
-          weather: data.weather,
-          windSpeed: data.wind.speed,
-          humidity: data.main.humidity,
-          sunrise: data.sys.sunrise,
-          sunset: data.sys.sunset,
-          pressure: data.main.pressure
-        };
-
-        meteoContainer.appendChild(forecastSummary);
-        console.log(data)
+        newList.appendChild(searchResult);
+        meteoContainer.appendChild(newList);
       })
       .catch(err => {
         console.error('Erreur:', err);
@@ -41,35 +69,45 @@ cityInput.addEventListener("input", (event) => {
   }
 });
 
+
 // Pour la construction du html
-window.weatherAPI.getCurrentWeatherByCity("Colmar")
-  .then(data => {
-    const forecastSummary = document.createElement('forecast-summary');
+// window.weatherAPI.getCurrentWeatherByCity("Colmar")
+//   .then(data => {
+//     const forecastSummary = document.createElement('forecast-summary');
 
-    forecastSummary.data = {
-      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-      currentTemp: data.main.temp,
-      feelsLike: data.main.feels_like,
-      cityName: data.name,
-      weather: data.weather,
-      windSpeed: data.wind.speed,
-      humidity: data.main.humidity,
-      sunrise: data.sys.sunrise,
-      sunset: data.sys.sunset,
-      pressure: data.main.pressure
-    };
+//     forecastSummary.data = {
+//       icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+//       currentTemp: data.main.temp,
+//       feelsLike: data.main.feels_like,
+//       cityName: data.name,
+//       weather: data.weather,
+//       windSpeed: data.wind.speed,
+//       humidity: data.main.humidity,
+//       sunrise: data.sys.sunrise,
+//       sunset: data.sys.sunset,
+//       pressure: data.main.pressure,
+//       country: data.sys.country
+//     };
 
-    meteoContainer.appendChild(forecastSummary);
-    console.log(data)
-  })
-  .catch(err => {
-    console.error('Erreur:', err);
-  });
+//     meteoContainer.appendChild(forecastSummary);
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.error('Erreur:', err);
+//   });
 
-  window.weatherAPI.getFiveDaysForecastByCity("Colmar")
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => {
-      console.error('Erreur:', err);
-    });
+// window.weatherAPI.getFiveDaysForecastByCity("Colmar")
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.error('Erreur:', err);
+//   });
+
+// window.weatherAPI.fetchCoordinatesTest("Herrlisheim")
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.error('Erreur:', err);
+//   });
