5073081845933abc09d5a49ab0fd21e1754ddac0
Dylan-Ay <dylan.ayache.68@gmail.com>
2025-11-18 15:23:21 +0100

    Update de HourlyForecast selon la nouvelle sémantique + ajout des données

--- renderer/components/HourlyForecast.js
+++ renderer/components/HourlyForecast.js
@@ -1,39 +1,36 @@
+import { epochToLocaleTimeString } from '../../utils/functions.js';
 class HourlyForecast extends HTMLElement {
     #data;
 
     constructor() {
         super();
-        this.wrapper = document.createElement('ul');
     }
     
     // Ajout de l'élement au DOM
     connectedCallback() {
-        this.appendChild(this.wrapper);
+
     }
 
     // Setter qui enregistre les données dans le composant et met à jour le DOM automatiquement
     set data(value) {
-        console.log('test')
         this.#data = value;
         this.render();
     }
 
     render() {
         if (!this.#data) {
-            this.wrapper.innerHTML = `<p>Pas de données</p>`;
+            this.innerHTML = `<p>Pas de données</p>`;
             console.log('Pas de données reçu dans le composant ForecastSummary');
             return;
         }
         
         const data = this.#data;
-        console.log(data)
-     
-        this.wrapper.innerHTML = `
-        <li>
-            <img src="${this.#data.icon}">
-            <p>${this.#data.currentTemp}°C</p>
-            <p>Ressenti : ${this.#data.feelsLike}°C</p>
-        </li>
+        
+        this.innerHTML = `
+            <span>${epochToLocaleTimeString(data.time, data.timeZone)}</span>
+            <img src="${data.icon}">
+            <p>${data.currentTemp.toFixed(0)}°C</p>
+            <p>Ressenti : ${data.feelsLike.toFixed(0)}°C</p>
     `;
     }
 }
