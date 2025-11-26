2af5518d1a95f395921a22167c6bed6a757a22ae
Dylan-Ay <dylan.ayache.68@gmail.com>
2025-11-26 20:21:18 +0100

    Update DailyForecast

--- renderer/components/DailyForecast.js
+++ renderer/components/DailyForecast.js
@@ -35,17 +35,17 @@ class DailyForecast extends HTMLElement {
                     <span class="text-sm">${weatherDesc.charAt(0).toUpperCase() + weatherDesc.slice(1)}</span>
                 </div>
                 <div class="flex items-center">
-                    <img src="${data.icon}" alt="Weather icon" class="w-15">
-                    <div class="flex flex-col text-end mr-4">
-                        <span class="font-semibold">${data.maxTemp.toFixed()}°</span>
+                    <img src="${data.icon}" alt="Weather icon" class="w-14">
+                    <div class="flex flex-col text-end text-sm gap-1.5">
+                        <span class="font-bold">${data.maxTemp.toFixed()}°</span>
                         <span>${data.minTemp.toFixed()}°</span>
                     </div>
-                    <div class="flex flex-col gap-1.5">
-                        <div class="flex items-center">
+                    <div class="flex flex-col gap-1.5 flex-1">
+                        <div class="flex items-center self-end">
                             <img class="dark:filter-[invert(1)] w-5" src="../src/icons/sunset.svg" alt="icon sunset">
                             <span class="text-sm">${epochToLocaleTimeString(data.sunset, data.timezone)}</span>
                             </div>
-                        <div class="flex items-center">
+                        <div class="flex items-center self-end">
                             <img class="dark:filter-[invert(1)] w-5" src="../src/icons/sunrise.svg" alt="icon sunrise">
                             <span class="text-sm">${epochToLocaleTimeString(data.sunrise, data.timezone)}</span>
                         </div>
